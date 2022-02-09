3ca36048b9da0380e03e781450b78908555e5369
xaniar87 <zaniar.ardalan@gmail.com>
2022-02-08 23:53:53 -0700

    wordladder done

--- WordLadder/src/wordladder.cpp
+++ WordLadder/src/wordladder.cpp
@@ -30,7 +30,6 @@ int main() {
 void find(string w1, string w2){
     Set<string> dictionary=dict();
     cout << "word1: "<< w1 << " word2: "<< w2<<endl;
-
 //    Create an empty queue of stacks.
     Queue<Stack<string>> queueStack;
 //    Create/add a stack containing {w1} to the queue.
@@ -93,20 +92,4 @@ Set<string> dict(){
     }
     return dictionary;
 }
-/***
- * Finding a word ladder between words w1 and w2:
-    Create an empty queue of stacks.
-    Create/add a stack containing {w1} to the queue.
-    While the queue is not empty:
-        Dequeue the partial-ladder stack from the front of the queue.
-        For each valid English word that is a "neighbor" (differs by 1 letter)
-        of the word on top of the stack:
-            If that neighbor word has not already been used in a ladder before:
-                If the neighbor word is w2:
-                    Hooray! we have found a solution.
-                Otherwise:
-                    Create a copy of the current partial-ladder stack.
-                    Put the neighbor word on top of the copy stack.
-                    Add the copy stack to the end of the queue.
- *
- * **/
+
