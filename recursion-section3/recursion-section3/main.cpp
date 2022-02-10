f75845c71eeb4d5606ff840e92adda9f51d6cadf
xaniar87 <zaniar.ardalan@gmail.com>
2022-02-10 01:21:47 -0700

    evaluate recursion done

--- recursion-section3/recursion-section3/main.cpp
+++ recursion-section3/recursion-section3/main.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 
 string writeChars(int n);
+int evaluate(string st);
+int evaluateHelper(string st, int& ind);
+
+
 int main()
 {
-    for (int i=0;i<10;i++){
-        cout << writeChars(i)<< endl;
-    }
-
+//    for (int i=0;i<10;i++){
+//        cout << writeChars(i)<< endl;
+//    }
+    cout << evaluate("((1+2)+(5+9))")<<endl;
     return 0;
 }
 
@@ -22,5 +26,35 @@ string writeChars(int n) {
         return "**";
     }else{
         return "<"+writeChars(n-2)+">";
+    }
+}
+
+
+int evaluate(string st){
+    int ind=0;
+    return evaluateHelper(st,ind);
+}
+int evaluateHelper(string st, int& ind){
+    cout << "st: "<< st <<endl;
+    if(st[ind]=='('){
+        ind++;
+        int left=evaluateHelper(st,ind);
+        cout << "left: "<<left<<endl;
+        ind++;
+        char opr=st[ind];
+        cout << "opr: "<<opr<<endl;
+        ind++;
+        int right=evaluateHelper(st,ind);
+        cout << "right: "<<right<<endl;
+
+        ind++;
+
+        if(opr=='+'){
+            return left+right;
+        }else{
+            return left*right;
+        }
+    }else{
+        return st[ind]-'0';
     }
 }
